36ff3ceb061fbc68413ed9f7e661b5eaed420fba
Mona-Deng <50858294+Mona-Deng@users.noreply.github.com>
2019-05-22 14:40:49 -0400

    Create reverse-list-rec.h

---
+++ reverse-list-rec.h
@@ -0,0 +1,8 @@
+#include "llist.h"
+
+/*
+ * reverse_rec(list) reverses the order of [list]. The algorithm is recursive.
+ * effects: mutates [list]
+ * time:    O(n), where n is the length of [list]
+ */
+void reverse_rec(struct llist *list);
