618d3cdfbe14000b7beeb8460e7ba42a0848dda6
Mona-Deng <50858294+Mona-Deng@users.noreply.github.com>
2019-05-22 14:39:58 -0400

    Create reverse-list-iter.h

---
+++ reverse-list-iter.h
@@ -0,0 +1,8 @@
+#include "llist.h"
+
+/*
+ * reverse_iter(list) reverses the order of [list]. The algorithm is iterative.
+ * effects: mutates [list]
+ * time:    O(n), where n is the length of [list]
+ */
+void reverse_iter(struct llist *list);
